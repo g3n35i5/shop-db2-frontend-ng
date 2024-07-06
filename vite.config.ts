5877d3b02ca5d84021d3c3112e4850b3f8ee28c0
Jan-Frederik Schmidt <janfschmidt@mailbox.org>
2024-07-06 11:11:20 +0200

    test: Setup `vitest` and create example test

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import istanbul from "vite-plugin-istanbul";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    istanbul({
+      include: ["src/**/*"], // files to track coverage on
+      exclude: ["node_modules"], // files to NOT track coverage on
+      requireEnv: false,
+    }),
+  ],
+});
---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    include: ["src/__tests__/**/*"],
+    coverage: {
+      provider: "v8",
+      include: ["src/helpers/"],
+      reportsDirectory: "coverage/unit/",
+      reporter: "lcov",
+    },
+  },
+});
