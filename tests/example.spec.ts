4374468af2974fce81b1bfcb5824917555ec8c79
Jan-Frederik Schmidt <janfschmidt@mailbox.org>
2024-07-07 09:21:59 +0200

    test: Setup `playwright` and add first test

---
+++ tests/example.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from "playwright-test-coverage";
+
+test("has title", async ({ page }) => {
+  await page.goto("/");
+
+  await expect(page.getByRole("heading", { level: 1 })).toHaveText(
+    "Vite + React"
+  );
+});
